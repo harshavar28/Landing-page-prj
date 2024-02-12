15f1ef930f5e6c7ff1031096f0f63fe2d829be8f
harshavar28 <113096344+harshavar28@users.noreply.github.com>
2024-02-12 07:55:36 +0000

    pricing landing page

---
+++ script.js
@@ -0,0 +1,49 @@
+const exchangeRate = 83.012;
+
+function choosePlan(plan) {
+    alert(`You have chosen the ${plan} plan!`);
+    
+}
+
+function selectCurrency(currency) {
+    document.getElementById('currencyDropdownBtn').innerText = currency;
+    updatePrices();
+}
+
+function selectUnit(unit) {
+    document.getElementById('unitDropdownBtn').innerText = unit;
+    updatePrices();
+}
+
+function updatePrices() {
+    const currencySelect = document.getElementById('currencyDropdownBtn');
+    const unitSelect = document.getElementById('unitDropdownBtn');
+
+    const basicPrice = document.getElementById('basic-price');
+    const proPrice = document.getElementById('pro-price');
+    const premiumPrice = document.getElementById('premium-price');
+
+    const currencySymbol = currencySelect.innerText;
+    const unitText = unitSelect.innerText;
+
+    
+    const basicPriceInINR = 19.99;
+    const proPriceInINR = 39.99;
+    const premiumPriceInINR = 59.99;
+
+    let basicPriceConverted, proPriceConverted, premiumPriceConverted;
+
+    if (currencySymbol === '$') {
+        basicPriceConverted = basicPriceInINR / exchangeRate;
+        proPriceConverted = proPriceInINR / exchangeRate;
+        premiumPriceConverted = premiumPriceInINR / exchangeRate;
+    } else if (currencySymbol === '₹') {
+        basicPriceConverted = basicPriceInINR ;
+        proPriceConverted = proPriceInINR ;
+        premiumPriceConverted = premiumPriceInINR ;
+    }
+
+    basicPrice.textContent = `${currencySymbol}${basicPriceConverted.toFixed(2)}/${unitText}`;
+    proPrice.textContent = `${currencySymbol}${proPriceConverted.toFixed(2)}/${unitText}`;
+    premiumPrice.textContent = `${currencySymbol}${premiumPriceConverted.toFixed(2)}/${unitText}`;
+}
